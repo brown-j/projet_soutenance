9570df4826a04d09f64f3d27172a22e42de608c9
Brown <jorkaelfoumbe@gmail.com>
2026-02-22 00:37:29 +0100

    ajout du fichier upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from celery_worker import app as celery_app
 # Ajoute ceci au début de ton fichier app.py, après les imports
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER
+    os.makedirs(UPLOAD_FOLDER)
 
 app = Flask(__name__)
 app.secret_key = "super_secret_key_2026"
